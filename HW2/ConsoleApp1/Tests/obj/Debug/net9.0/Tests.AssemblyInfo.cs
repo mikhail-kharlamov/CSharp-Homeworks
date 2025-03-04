d580c9192a820438309a17d2fe3fc796acedb83e
michaelharlam <palptin2014@gmail.com>
2025-03-05 02:26:52 +0300

    Trie data structure is done

--- HW2/ConsoleApp1/Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
+++ HW2/ConsoleApp1/Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6459baaac3b45e11b3614fdf1e22cd511c6db36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f86f7b5ed3423adfc8b94042d5273bb3332e24e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
