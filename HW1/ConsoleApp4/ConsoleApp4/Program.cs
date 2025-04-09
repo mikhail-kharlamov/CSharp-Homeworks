55f11a0ebc4d868ea2a0278ba64e6f4706ce676e
michaelharlam <palptin2014@gmail.com>
2025-04-10 02:19:23 +0300

    deleted working comments and other home- and class- works

--- HW1/ConsoleApp4/ConsoleApp4/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using BurrowsWheeler;
-
-var (transformed, pos) = BurrowsWheelerTransform.StraightTransform("abacaba");
-Console.WriteLine(transformed);
-Console.WriteLine(pos);
-var text = BurrowsWheelerTransform.InverseTransform(transformed, pos);
-Console.WriteLine(text);
